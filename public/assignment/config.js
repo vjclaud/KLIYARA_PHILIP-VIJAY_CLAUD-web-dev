cfcf390967ff3c7b715a3c539e27b5428d59d0e6
vijay claud kliyara philip <vjclaudkp@gmail.com>
2016-05-26 21:07:51 -0400

    A3: Finised complete navigation

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -14,5 +14,41 @@
             .when("/profile", {
                 templateUrl : "views/user/profile.view.client.html"
             })
+
+            .when("/page-edit", {
+                templateUrl : "views/page/page-edit.view.client.html"
+            })
+            .when("/page-list", {
+                templateUrl : "views/page/page-list.view.client.html"
+            })
+            .when("/page-new", {
+                templateUrl : "views/page/page-new.view.client.html"
+            })
+
+            .when("/website-edit", {
+                templateUrl : "views/website/website-edit.view.client.html"
+            })
+            .when("/website-list", {
+                templateUrl : "views/website/website-list.view.client.html"
+            })
+            .when("/website-new", {
+                templateUrl : "views/website/website-new.view.client.html"
+            })
+
+            .when("/widget-chooser", {
+                templateUrl : "views/widget/widget-chooser.view.client.html"
+            })
+            .when("/widget-heading", {
+                templateUrl : "views/widget/widget-heading.view.client.html"
+            })
+            .when("/widget-image", {
+                templateUrl : "views/widget/widget-image.view.client.html"
+            })
+            .when("/widget-list", {
+                templateUrl : "views/widget/widget-list.view.client.html"
+            })
+            .when("/widget-youtube", {
+                templateUrl : "views/widget/widget-youtube.view.client.html"
+            })
     }
 })();
