1d9047e91612ec6e6c842252818a2b62aac4db47
vijay claud kliyara philip <vjclaudkp@gmail.com>
2016-06-02 19:30:54 -0400

    A4: created required server files

---
+++ assignment/services/page.service.server.js
@@ -0,0 +1,10 @@
+module.exports = function(app) {
+
+    var pages = [
+        { "_id": "321", "name": "Post 1", "websiteId": "456" },
+        { "_id": "432", "name": "Post 2", "websiteId": "456" },
+        { "_id": "543", "name": "Post 3", "websiteId": "456" }
+    ]
+
+    
+};
---
+++ assignment/services/website.service.server.js
@@ -0,0 +1,12 @@
+module.exports = function(app) {
+
+    var websites = [
+        { "_id": "123", "name": "Facebook",  "developerId": "456" },
+        { "_id": "234", "name": "Tweeter",   "developerId": "456" },
+        { "_id": "456", "name": "Gizmodo",  "developerId": "456" },
+        { "_id": "567", "name": "Tic Tac Toe", "developerId": "123" },
+        { "_id": "678", "name": "Checkers",   "developerId": "123" },
+        { "_id": "789", "name": "Chess",  "developerId": "234" }
+    ]
+    
+};
