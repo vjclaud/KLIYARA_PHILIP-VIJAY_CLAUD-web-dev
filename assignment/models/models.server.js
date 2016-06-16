a20e6be7a8487b7dc3bf47a9f5e209ba24e0a0dc
vijay claud kliyara philip <vjclaudkp@gmail.com>
2016-06-16 19:06:44 -0400

    server fix

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -1,7 +1,20 @@
 module.exports = function(){
 
     var mongoose = require('mongoose');
-    var connection = mongoose.connect('mongodb://localhost/webdev');
+
+    var connectionString = 'mongodb://localhost/webdev';
+
+    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+
+    var mongoose = require("mongoose");
+    mongoose.connect(connectionString);
+    
     var models = {
         userModel: require("./user/user.model.server")(),
         websiteModel: require("./website/website.model.server")(),
